1a172d31538e8f122cf5fc23e96d183ce00e0f3c
Or Dvir <or_dvir@hotmail.com>
2024-03-08 15:56:46 +0100

    showing message dialog for one-time messaages

--- app/src/main/java/com/hotmail/or_dvir/arisquiz/ui/categoriesScreen/CategoriesScreen.kt
+++ app/src/main/java/com/hotmail/or_dvir/arisquiz/ui/categoriesScreen/CategoriesScreen.kt
@@ -3,15 +3,21 @@ package com.hotmail.or_dvir.arisquiz.ui.categoriesScreen
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -29,13 +35,13 @@ fun CategorySelectionScreen(
     navigator: DestinationsNavigator,
     viewModel: CategoriesViewModel = koinViewModel()
 ) {
-    LaunchedEffect(Unit) {
+    var messageToUser by remember { mutableStateOf("") }
+
+    LaunchedEffect(viewModel.categoriesOneTimeEvents) {
         viewModel.apply {
             categoriesOneTimeEvents.collectLatest {
                 when (it) {
-                    is CategoriesOneTimeEvents.Message -> {
-                        //todo
-                    }
+                    is CategoriesOneTimeEvents.Message -> messageToUser = it.message
                 }
             }
         }
@@ -43,12 +49,26 @@ fun CategorySelectionScreen(
 
     val state by viewModel.categoriesState.collectAsStateWithLifecycle()
 
-    Column {
-        Text("${state.categories.size} categories")
+    Box {
+        Column {
+            Text("${state.categories.size} categories")
 
-        // this must be last in the colum to show above everything else
-        if (viewModel.isLoading) {
-            FullScreenProgressIndicator()
+            // this must be last in the colum to show above everything else
+            if (viewModel.isLoading) {
+                FullScreenProgressIndicator()
+            }
+        }
+
+        if(messageToUser.isNotBlank()) {
+            AlertDialog(
+                onDismissRequest = { messageToUser = "" },
+                confirmButton = {
+                    TextButton(onClick = { messageToUser = "" }) {
+                        Text(stringResource(android.R.string.ok))
+                    }
+                },
+                text = { Text(messageToUser) }
+            )
         }
     }
 }

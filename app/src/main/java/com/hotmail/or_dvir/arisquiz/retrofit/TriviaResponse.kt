121565185df8bacc5c4e61f1f45039bca4de829f
Or Dvir <or_dvir@hotmail.com>
2024-03-08 15:56:02 +0100

    added some documentation

--- app/src/main/java/com/hotmail/or_dvir/arisquiz/retrofit/TriviaResponse.kt
+++ app/src/main/java/com/hotmail/or_dvir/arisquiz/retrofit/TriviaResponse.kt
@@ -2,10 +2,32 @@ package com.hotmail.or_dvir.arisquiz.retrofit
 
 sealed class TriviaResponse<T>(val responseCode: Int, val data: T? = null) {
     class Success<T>(data: T) : TriviaResponse<T>(CODE_SUCCESS, data)
+
+    /**
+     * Could not return results. The API doesn't have enough questions for your query.
+     * (Ex. Asking for 50 Questions in a Category that only has 20.)
+     */
     class NoResult<T> : TriviaResponse<T>(CODE_NO_RESULT)
+
+    /**
+     * Contains an invalid parameter. Arguments passed in aren't valid. (Ex. Amount = Five)
+     */
     class InvalidParameter<T> : TriviaResponse<T>(CODE_INVALID_PARAMETER)
+
+    /**
+     * Session Token does not exist.
+     */
     class TokenNotFound<T> : TriviaResponse<T>(CODE_TOKEN_NOT_FOUND)
+
+    /**
+     * Session Token has returned all possible questions for the specified query.
+     * Resetting the Token is necessary.
+     */
     class TokenEmpty<T> : TriviaResponse<T>(CODE_TOKEN_EMPTY)
+
+    /**
+     * Too many requests have occurred. Each IP can only access the API once every 5 seconds
+     */
     class RateLimit<T> : TriviaResponse<T>(CODE_RATE_LIMIT)
 
     class Exception<T> : TriviaResponse<T>(CODE_EXCEPTION)
@@ -20,7 +42,6 @@ sealed class TriviaResponse<T>(val responseCode: Int, val data: T? = null) {
         private const val CODE_RATE_LIMIT = 5
         private const val CODE_EXCEPTION = -1
 
-        // todo investigate more about these response codes... the api claims to attach these, but where?!
         fun <T> fromResponseCode(code: Int, data: T, message: String?): TriviaResponse<T> =
             when (code) {
                 0, 200 -> Success(data)
